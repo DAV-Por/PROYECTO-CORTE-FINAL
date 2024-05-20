8c1deba8b2717a897ad841c31b4bdc2f7fdd847d
Juan <jd.portilla10@ciaf.edu.co>
2024-05-19 19:45:50 -0500

    Se sube las clases de inventario y producto

---
+++ src/Producto.java
@@ -0,0 +1,59 @@
+package src;
+
+public class Producto {
+    String nombre;
+    int id, precio, stock;
+    String descripcion;
+
+    public Producto() {
+    }
+
+    public Producto(String nombre, int id, int precio, int stock, String descripcion) {
+        this.id = id;
+        this.precio = precio;
+        this.stock = stock;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+}
