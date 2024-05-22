ad6e5e7c718643ae162e27b14f5bb27ec3a65ae6
cesaravc2713 <ca.vargas27@ciaf.edu.co>
2024-05-21 22:48:27 -0500

    e

---
+++ src/GestionPedidos.java
@@ -0,0 +1,46 @@
+package src;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GestionPedidos {
+    private Map<Integer, Pedido> pedidos;
+
+    public GestionPedidos() {
+        this.pedidos = new HashMap<>();
+    }
+
+    public void agregarPedido(Pedido pedido) {
+        this.pedidos.put(pedido.getIdPedido(), pedido);
+    }
+
+    public void enviarPedido(int idPedido) {
+        Pedido pedido = this.pedidos.get(idPedido);
+        if (pedido != null) {
+            pedido.cambiarEstado("enviado");
+            System.out.println("rl pedido con id: " + idPedido + " ha sido enviado.");
+        } else {
+            System.out.println("no se encontrooel pedido : " + idPedido);
+        }
+    }
+
+    public void entregarPedido(int idPedido) {
+        Pedido pedido = this.pedidos.get(idPedido);
+        if (pedido != null) {
+            pedido.cambiarEstado("entregado");
+            System.out.println("el pedido con id: " + idPedido + " ha sido entregado.");
+        } else {
+            System.out.println("no se encontro el pedido : " + idPedido);
+        }
+    }
+
+    public void devolverPedido(int idPedido) {
+        Pedido pedido = this.pedidos.get(idPedido);
+        if (pedido != null) {
+            pedido.cambiarEstado("devuelto");
+            System.out.println("el pedido con id: " + idPedido + " ha sido devuelto.");
+        } else {
+            System.out.println("no se encontro el pedido : " + idPedido);
+        }
+    }
+}
