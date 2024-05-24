f76cb1c1d855fb52336513a3d6a8e0b32bb12c03
Juan <jd.portilla10@ciaf.edu.co>
2024-05-24 18:57:56 -0500

    pa desarrollo

--- src/ClientesYProveedores.java
+++ src/ClientesYProveedores.java
@@ -4,22 +4,19 @@ import java.util.HashMap;
 import java.util.ArrayList;
 
 public class ClientesYProveedores {
-    public class Cliente{
+    public class Cliente {
+        private Map<Integer, Pedido> clientes = new HashMap<>();
+
         private int ID;
         private String nombre;
         private String apellido;
         private int edad;
         private String correo;
-        private String  telefono;
+        private String telefono;
         private String direccion;
         private Pedido pedido;
 
-        public Cliente() {
-        }
-
-        public Map<Integer, Pedido> Clientes = new HashMap<Integer, Pedido>();
-
-        public Cliente(int ID, String nombre, String apellido, int edad, String correo, String  telefono, String direccion, Pedido pedido) {
+        public Cliente(int ID, String nombre, String apellido, int edad, String correo, String telefono, String direccion, Pedido pedido) {
             this.ID = ID;
             this.nombre = nombre;
             this.apellido = apellido;
@@ -27,9 +24,7 @@ public class ClientesYProveedores {
             this.correo = correo;
             this.telefono = telefono;
             this.direccion = direccion;
-            Clientes = new HashMap<Integer, Pedido>();
             this.pedido = pedido;
-            listaClientes();
         }
 
         public void listaClientes () {
@@ -37,14 +32,19 @@ public class ClientesYProveedores {
             Cliente cliente01 = new Cliente(01, "Esteban", "Quito", 36, "estabanquito36@gmail.com", "3147964650", "Calle Quito 33", pedido);
             Cliente cliente02 = new Cliente(02, "Juan Jose", "Estrada", 45, "joseestrada34@gmail.com", "3205216478", "Calle 4# 33-44", pedido);
             Cliente cliente03 = new Cliente(03, "Johan", "Castañeda", 21, "elmaspaspi33@gmail.com", "3147951243", "Carrea 6#26-33", pedido);
-            Clientes.put(cliente01.ID, pedido); Clientes.put(cliente02.ID, pedido); Clientes.put(cliente03.ID, pedido);
+            clientes.put(cliente01.ID, pedido); clientes.put(cliente02.ID, pedido); clientes.put(cliente03.ID, pedido);
         }
 
         public void eliminarCliente(int ID) {
-            Clientes.remove(ID);
+            if (clientes.containsKey(ID)) {
+                clientes.remove(ID);
+            }
         }
 
-
-
+        public void agregarCliente(Cliente cliente) {
+            if (!clientes.containsKey(cliente.ID)) {
+                clientes.put(cliente.ID, cliente);
+            }
+        }
     }
 }
